4800735f4a88fd2d748967d4c8ded33b7f130154
Ohze5 <rafaelsapand@gmail.com>
2026-04-25 22:00:48 +0100

    Added a Report New Fault Button which releases a drop down selection menu

--- frontend-ar/src/views/LoggedInView.jsx
+++ frontend-ar/src/views/LoggedInView.jsx
@@ -1,6 +1,10 @@
 import FaultItem from "../components/FaultItem";
+import { useState } from "react";
 
-function LoggedInView({ faults, logout, severityColors }) {
+function LoggedInView({ faults, logout }) {
+  const [showReportForm, setShowReportForm] = useState(false);
+  const [severity, setSeverity] = useState("");
+
   return (
     <div className="fault-list">
       <h2>Active Transport Faults</h2>
@@ -15,12 +19,51 @@ function LoggedInView({ faults, logout, severityColors }) {
         )}
       </ul>
 
-      <button
-        className="logout"
-        onClick={logout}
-      >
-        Log Out
-      </button>
+      <div className="fault-footer">
+        <button className="logout" onClick={logout}>
+          Log Out
+        </button>
+
+        <button
+          className="report-fault"
+          onClick={() => setShowReportForm(prev => !prev)}
+        >
+          Report New Fault
+        </button>
+      </div>
+
+      {showReportForm && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+
+            <h3>Report New Fault</h3>
+
+            <input placeholder="Location" />
+            <input placeholder="Type" />
+
+            <select value={severity} onChange={(e) => setSeverity(e.target.value)}>
+              <option value ="" disabled>
+                  Select Severity Level
+              </option>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+              <option>Very High</option>
+            </select>
+
+            <div className="modal-actions">
+                <button onClick={() => setShowReportForm(false)}>
+                    Cancel
+                </button>
+
+                <button onClick={() => console.log("Submit Fault")}>
+                    Submit
+                </button>
+            </div>
+
+          </div>
+        </div>
+      )}
     </div>
   );
 }
